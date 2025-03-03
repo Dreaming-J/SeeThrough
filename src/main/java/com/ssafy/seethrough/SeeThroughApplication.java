cf661924053ab965c16f8c150cf24a08d5c4afdf
김종헌 <kimjjong321@naver.com>
2025-03-03 19:24:53 +0900

    :tada: Init. Spring Boot 초기 세팅

---
+++ src/main/java/com/ssafy/seethrough/SeeThroughApplication.java
@@ -0,0 +1,14 @@
+package com.ssafy.seethrough;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+public class SeeThroughApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeeThroughApplication.class, args);
+    }
+
+}
