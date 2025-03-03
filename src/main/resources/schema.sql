baa469c029dd18754c0145aed09d71b8d3eb275f
김종헌 <kimjjong321@naver.com>
2025-03-03 20:16:53 +0900

    :card_file_box: Chore. 테이블 이름 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,23 +1,17 @@
+DROP DATABASE IF EXISTS see_through;
+CREATE DATABASE see_through;
 USE see_through;
 
-DROP TABLE IF EXISTS refrigerator_logs;
-DROP TABLE IF EXISTS refrigerator_inventory;
-DROP TABLE IF EXISTS food_categories;
-DROP TABLE IF EXISTS member_logs;
-DROP TABLE IF EXISTS member_images;
-DROP TABLE IF EXISTS member;
-
-
 -- 사용자 정보 테이블
-CREATE TABLE member (
-                        id VARCHAR(36) NOT NULL COMMENT 'UUID',
-                        name VARCHAR(100) NULL DEFAULT 'Undefined' COMMENT '랜덤 닉네임',
-                        is_identified TINYINT(1) NOT NULL DEFAULT FALSE COMMENT '구성원 등록 여부',
-                        created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                        recognition_times INTEGER NOT NULL DEFAULT 0,
-                        significants JSON NULL COMMENT '특이사항을 JSON배열 형식으로 저장',
+CREATE TABLE members (
+                         id VARCHAR(36) NOT NULL COMMENT 'UUID',
+                         name VARCHAR(100) NULL DEFAULT 'Undefined' COMMENT '랜덤 닉네임',
+                         is_identified TINYINT(1) NOT NULL DEFAULT FALSE COMMENT '구성원 등록 여부',
+                         created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                         recognition_times INTEGER NOT NULL DEFAULT 0,
+                         significants JSON NULL COMMENT '특이사항을 JSON배열 형식으로 저장',
 
-                        PRIMARY KEY (id)
+                         PRIMARY KEY (id)
 );
 
 -- 얼굴 이미지 테이블
@@ -28,7 +22,7 @@ CREATE TABLE member_images (
                                created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
                                PRIMARY KEY (id),
-                               FOREIGN KEY (member_id) REFERENCES member(id)
+                               FOREIGN KEY (member_id) REFERENCES members(id)
 );
 
 -- 얼굴 인식 로그 테이블
@@ -38,28 +32,28 @@ CREATE TABLE member_logs (
                              created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
                              PRIMARY KEY (id),
-                             FOREIGN KEY (member_id) REFERENCES member(id)
+                             FOREIGN KEY (member_id) REFERENCES members(id)
 );
 
 -- 식품 카테고리 테이블
-CREATE TABLE food_categories (
-                                 id INTEGER NOT NULL AUTO_INCREMENT,
-                                 name VARCHAR(100) NOT NULL,
+CREATE TABLE food_category (
+                               id INTEGER NOT NULL AUTO_INCREMENT,
+                               name VARCHAR(100) NOT NULL,
 
-                                 PRIMARY KEY (id)
+                               PRIMARY KEY (id)
 );
 
 -- 냉장고 재고 테이블
 CREATE TABLE refrigerator_inventory (
                                         id VARCHAR(36) NOT NULL COMMENT 'UUID',
                                         member_id VARCHAR(36) NOT NULL COMMENT 'UUID',
-                                        food_categories_id INTEGER NOT NULL,
+                                        food_category_id INTEGER NOT NULL,
                                         inbound_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
                                         expiration_at TIMESTAMP NULL COMMENT '소비 기한',
 
                                         PRIMARY KEY (id),
-                                        FOREIGN KEY (member_id) REFERENCES member (id),
-                                        FOREIGN KEY (food_categories_id) REFERENCES food_categories (id)
+                                        FOREIGN KEY (member_id) REFERENCES members (id),
+                                        FOREIGN KEY (food_category_id) REFERENCES food_category (id)
 );
 
 -- 냉장고 입출고 로그 테이블
@@ -72,5 +66,5 @@ CREATE TABLE refrigerator_logs (
 
                                    PRIMARY KEY (id),
                                    FOREIGN KEY (refrigerator_inventory_id) REFERENCES refrigerator_inventory(id),
-                                   FOREIGN KEY (member_id) REFERENCES member(id)
+                                   FOREIGN KEY (member_id) REFERENCES members(id)
 );
